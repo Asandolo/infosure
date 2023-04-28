66fec0dc52c9440804d267a84a44136f651884dd
Brownie <brownie@42.fr>
2023-04-28 15:10:21 +0200

    🎨 style(helpbox.tsx): replace anchor tag with div tag for better semantics and accessibility

--- src/componants/helpbox.tsx
+++ src/componants/helpbox.tsx
@@ -26,14 +26,14 @@ export function HelpBox () {
         <>
             <HelpButton />
             <div id='helpbox' className="absolute top-4 left-2 hidden">
-                <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Fonctionnement:</h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">Le site infosu.re donne 50% du temps</p>
                     <ul>
                         <li className="font-normal text-gray-700 dark:text-gray-400"> - "Info sure !"</li>
                         <li className="font-normal text-gray-700 dark:text-gray-400"> - "Info pas sure !"</li>
                     </ul>
-                </a>
+                </div>
             </div>
         </>
     )
